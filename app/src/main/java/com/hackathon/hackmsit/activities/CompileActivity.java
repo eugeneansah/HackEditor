d9baf532ffb6bd79a2eb4a4b79a61d316ec339d5
joydeepsaha05 <joydeepsaha05@gmail.com>
2016-02-04 03:36:24 +0530

    Code highlighting - while Ishaan.sleep(true)

--- app/src/main/java/com/hackathon/hackmsit/activities/CompileActivity.java
+++ app/src/main/java/com/hackathon/hackmsit/activities/CompileActivity.java
@@ -3,21 +3,35 @@ package com.hackathon.hackmsit.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.hackathon.hackmsit.R;
 
 public class CompileActivity extends AppCompatActivity {
-
+    EditText input;
+    Button testButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        testButton = (Button) findViewById(R.id.button);
+        input = (EditText) findViewById(R.id.inputText);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("aakash", "text is: " + input.getText());
+            }
+        });
     }
     /*@Override
     public void onBackPressed() {
